97287fed0809c99b97cd2da201c9a48f7111e32f
KarlaRosales13F <karla.rosalesc06@gmail.com>
2025-10-21 10:04:06 -0500

    Clase 21/10/2025

---
+++ modulo2/javascript/05_bucles.js
@@ -0,0 +1,23 @@
+for (let i = 1; i <= 10; i++) {
+    console.log("Iteración número: " + i);
+}
+
+let suma = 0;
+
+for (let i = 1; i <= 10; i++) {
+    console.log("Iteración número: " + i);
+    suma = suma + i;
+}
+console.log("Sumatoria indices: " + suma);
+
+console.log("Multiplicacion: ");
+let multiplicacion = 4;
+for (let i = 1; i <= 10; i++) {
+    console.log(multiplicacion + " x " + i + " = " + (multiplicacion * i));
+}
+
+console.log("sumatoria indices: ", suma);
+let nombre = "francisco";
+for (let i = 0; i <=nombre.length; i++){
+    console.log(nombre[i]);
+}
