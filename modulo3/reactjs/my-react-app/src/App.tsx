8022bd4a1f5cf37a2eb5ba215928062d71c5b417
KarlaRosales13F <karla.rosalesc06@gmail.com>
2025-12-11 10:01:33 -0500

    Clase 11-12-2025

--- modulo3/reactjs/my-react-app/src/App.tsx
+++ modulo3/reactjs/my-react-app/src/App.tsx
@@ -1,17 +1,53 @@
+import ButtonCounter from './components/ButtonCounter';
+import ColorBox from './components/ColorBox';
+import ConditionalText from './components/ConditionalText';
+import EmojiList from './components/EmojiList';
+import  HelloWorld  from './components/HelloWorld';
+import PromedioTresNotasForm from './components/PromedioTresNotasForm';
+import ShowDate from './components/ShowDate';
+import SimpleForm from './components/SimpleForm';
+import SumaForm from './components/SumaForm';
+import TextInput from './components/TextInput';
+import ToggleMessage from './components/ToggleMessage';
+import WelcomeUser from './components/WelcomeUser';
 import Greeting from './props/Greeting';
-import PromedioTresNotas from './components/PromedioTresNotasForm';
-import UserCard from './props/UseCard';
+import ParentButtonOne from './props/ParentButtonOne';
+import ParentIncrementButton from './props/ParentIncrementButton';
+import ParentInputNotifier from './props/ParentInputNotifier';
+import ParentLoginForm from './props/ParentLoginForm';
+import ParentUserList from './props/ParentUserList';
+import UserCard from './props/UserCard';
+import LoginWithLimit from './useState/LoginWithLimit';
 
 function App() {
-
-const userData = { name: "Carlos", age: 30, lastName: "Gomez", address: "Calle Falsa 123", phone: "555-1234" };
-
+  const userData = { name: "Carlos", age: 30,
+    apellido: "Gonzalez",
+    direccion: "Calle Falsa 123",
+    telefono: "555-1234",
+   };
   return (
-    <>
+      <>
+      <HelloWorld />
+      <WelcomeUser />
+      <ButtonCounter />
+      <TextInput />
+      <ToggleMessage />
+      <ShowDate />
+      <ColorBox />
+      <ConditionalText />
+      <EmojiList />
+      <SimpleForm />
+      <SumaForm />
       <Greeting name="Francisco" />
       <Greeting name="Ana" />
-      <PromedioTresNotas/>
-      <UserCard user={userData}/>
+      <PromedioTresNotasForm />
+      
+      <UserCard user={userData} />
+      <ParentButtonOne />
+      <ParentIncrementButton />
+      <ParentInputNotifier />
+      <ParentUserList />
+      <ParentLoginForm/>
     </>
   );
 }
---
+++ modulo3/reactjs/my-react-app/src/props/ButtonOne.tsx
@@ -0,0 +1,5 @@
+export default function ButtonOne(props) {
+  return <button onClick={props.onClick}>Haz clic aquí</button>;
+}export default function ButtonOne(props) {
+  return <button onClick={props.onClick}>Haz clic aquí</button>;
+}
